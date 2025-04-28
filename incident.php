b5d9e20ecdd1dda9c8ef3bf6d9ea21e9c241a2e4
Moha <mohammad.awad0110@gmail.com>
2025-04-28 08:51:16 +0200

    add incident.php

---
+++ incident.php
@@ -0,0 +1,22 @@
+<?php
+require_once('db.php');
+session_start();
+
+// Säkerställ att användaren är inloggad
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+}
+
+// Sätt titel
+$title = "Incidents";
+
+// Bestäm vilken content som ska inkluderas
+$content = "pages/incidents_content.php";
+
+// Hämta roll från sessionen (vi förutsätter att roll lagras vid login)
+$user_role = $_SESSION['role_name']; // Ex: 'Administrator', 'Reporter', 'Responder'
+
+// Lägg till roll i layouten
+include('layout/layout.php');
+?>
