e6f05394bc6f65b1488b15fdcb800920e1fbfe0c
Moha <mohammad.awad0110@gmail.com>
2025-05-12 11:08:33 +0200

    update, move php to incident.php

---
+++ incidents.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+require_once 'db.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php"); 
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+$role = $_SESSION['role'];
+
+if (isset($_SESSION['flash_message'])) {
+    echo '<div class="alert alert-success">' . $_SESSION['flash_message'] . '</div>';
+    unset($_SESSION['flash_message']);
+}
+
+$sql = "
+  SELECT i.*, iu.user_name, r.role_name, 
+  (SELECT st.status_type 
+   FROM incident_status ists 
+   JOIN status_type st ON ists.status_type_id = st.status_type_id
+   WHERE ists.inc_id = i.inc_id 
+   ORDER BY ists.reported_at DESC LIMIT 1) AS current_status
+  FROM incident i
+  JOIN incident_user iu ON i.inc_user_id = iu.inc_user_id
+  JOIN role r ON iu.role_id = r.role_id
+";
+
+if ($role == 'Reporter') {
+    $sql .= " WHERE i.inc_user_id = ? ";
+}
+$sql .= " ORDER BY i.inc_id DESC";
+$stmt = $conn->prepare($sql);
+if ($role == 'Reporter') {
+    $stmt->bind_param("i", $user_id);
+}
+
+
+$stmt->execute();
+$result = $stmt->get_result();
+$title = "Incidents";
+$content = "pages/incidents_content.php";
+
+include('layout/layout.php');
--- pages/incidents_content.php
+++ pages/incidents_content.php
@@ -3,42 +3,6 @@
     <h1><?= htmlspecialchars($title) ?></h1>
     <a href="dashboard.php" class="btn btn-secondary">Back</a>
   </div>
-
-  <?php
-  $user_id = $_SESSION['user_id'];
-  $role = $_SESSION['role'];
-
-  if (isset($_SESSION['flash_message'])) {
-    echo '<div class="alert alert-success">' . $_SESSION['flash_message'] . '</div>';
-    unset($_SESSION['flash_message']);
-  }
-
-  $sql = "
-  SELECT i.*, iu.user_name, r.role_name, 
-  (SELECT st.status_type 
-   FROM incident_status ists 
-   JOIN status_type st ON ists.status_type_id = st.status_type_id
-   WHERE ists.inc_id = i.inc_id 
-   ORDER BY ists.reported_at DESC LIMIT 1) AS current_status
-  FROM incident i
-  JOIN incident_user iu ON i.inc_user_id = iu.inc_user_id
-  JOIN role r ON iu.role_id = r.role_id
-";
-
-  if ($role == 'Reporter') {
-    $sql .= " WHERE i.inc_user_id = ? ";
-  }
-  $sql .= " ORDER BY i.inc_id DESC";
-  $stmt = $conn->prepare($sql);
-  if ($role == 'Reporter') {
-    $stmt->bind_param("i", $user_id);
-  }
-
-
-  $stmt->execute();
-  $result = $stmt->get_result();
-  ?>
-
   <?php if ($result->num_rows > 0): ?>
     <table class="table table-striped table-bordered table-hover mb-0">
       <thead>
