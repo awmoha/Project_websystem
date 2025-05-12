929743fb0666c404a31a449c78cdde7805e84f0f
Moha <mohammad.awad0110@gmail.com>
2025-05-12 11:15:29 +0200

    move php code to view.php

--- pages/view_content.php
+++ pages/view_content.php
@@ -1,63 +1,8 @@
-<?php
-if (!isset($_GET['id'])) {
-    header("Location: incidents.php");
-    exit();
-}
-
-
-require_once('db.php');
-session_start();
-
-$inc_id = intval($_GET['id']);
-$user_id = $_SESSION['user_id'];
-$role = $_SESSION['role'];
-
-$sql = "SELECT i.*, iu.user_name 
-        FROM incident i 
-        JOIN incident_user iu ON i.inc_user_id = iu.inc_user_id 
-        WHERE i.inc_id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $inc_id);
-$stmt->execute();
-$incident = $stmt->get_result()->fetch_assoc();
-
-$sql_comments = "SELECT c.*, iu.user_name 
-                 FROM comment c 
-                 JOIN incident_user iu ON c.inc_user_id = iu.inc_user_id 
-                 WHERE c.inc_id = ? ORDER BY c.created_at ASC";
-$stmt_comments = $conn->prepare($sql_comments);
-$stmt_comments->bind_param("i", $inc_id);
-$stmt_comments->execute();
-$comments = $stmt_comments->get_result();
-
-$sql_status_history = "SELECT s.*, st.status_type, iu.user_name
-                       FROM incident_status s
-                       JOIN status_type st ON s.status_type_id = st.status_type_id
-                       JOIN incident_user iu ON s.inc_user_id = iu.inc_user_id
-                       WHERE s.inc_id = ? ORDER BY s.reported_at ASC";
-$stmt_status = $conn->prepare($sql_status_history);
-$stmt_status->bind_param("i", $inc_id);
-$stmt_status->execute();
-$status_history = $stmt_status->get_result();
-
-$sql_images = "SELECT * FROM incident_evidence WHERE inc_id = ?";
-$stmt_images = $conn->prepare($sql_images);
-$stmt_images->bind_param("i", $inc_id);
-$stmt_images->execute();
-$images = $stmt_images->get_result();
-
-$status_options = [];
-$status_query = $conn->query("SELECT * FROM status_type");
-while ($row = $status_query->fetch_assoc()) {
-    $status_options[] = $row;
-}
-
-?>
 
 <div class="container mt-5">
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h1 class="mb-4">Incident #<?= htmlspecialchars($incident['inc_id']) ?></h1>
-        <a href="dashboard.php" class="btn btn-secondary">Back</a>
+        <a href="incidents.php" class="btn btn-secondary">Back</a>
     </div>
 
     <?php if (isset($_SESSION['flash_message'])): ?>
